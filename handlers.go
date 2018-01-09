d3270f5f57a28ec38f75294ca70bdac13d59e43c
Bruno Panuto <bl.panuto@gmail.com>
2018-01-09 20:49:17 -0200

    gofmt

--- handlers.go
+++ handlers.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 )
 
--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"os"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	mux := httprouter.New()
 	bc := NewBlockcypherAPI(os.Getenv("BLOCKCYPHER_API_TOKEN"), "btc", "main")
+
 	mux.POST("/addr/", createAddressHandler(bc))
 
 	server := &http.Server{
-		Addr: ":8080",
+		Addr:    ":8080",
 		Handler: mux,
 	}
 
